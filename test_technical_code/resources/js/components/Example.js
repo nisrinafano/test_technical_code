5389a85e58f0c4b89ee7461e29d5eaae8e046f3b
nisrinafano <19148635+nisrinafano@users.noreply.github.com>
2023-10-30 12:17:27 +0700

    add ajax, route
    - add ajax function to connect input form and backend
    - add route to backend function
    - fix form input, add result text

--- test_technical_code/resources/js/components/Example.js
+++ test_technical_code/resources/js/components/Example.js
@@ -8,10 +8,14 @@ function Example() {
                 <div className="col-md-8">
                     <div className="card">
                         <form>
-                            <input type="text" placeholder='Input Angka' name="angka"></input>
+                            <input type="text" placeholder='Input Angka' name="angka" id='input_angka'></input>
+                            <br></br>
                             <input type='button' id='btn_segitiga'>Generate segitiga</input>
                             <input type='button' id='btn_ganjil'>Generate Bilangan Ganjil</input>
                             <input type='button' id='btn_prima'>Generate Bilangan Prima</input>
+                            <br></br>
+                            <h1>Result:</h1>
+                            <p id='txt_hasil'></p>
                         </form>
                     </div>
                 </div>
@@ -24,4 +28,59 @@ export default Example;
 
 if (document.getElementById('example')) {
     ReactDOM.render(<Example />, document.getElementById('example'));
+}
+
+var base_url = '127.0.0.1:8000';
+document.getElementById('btn_segitiga').onclick = function () {
+    var input_num = document.getElementById('input_angka').value;
+
+    if (input_num === '') {
+        txt_hasil.innerHTML('Input tidak boleh kosong');
+    } else if (isNaN(input_num)) {
+        txt_hasil.innerHTML('Input harus berupa angka');
+    } else {
+        $.ajax({
+            url: base_url + '/api/segitiga',
+            type: 'GET',
+            dataType: 'json',
+            data: input_num,
+            success: (data) => txt_hasil.innerHTML(data)
+        })
+    }
+}
+
+document.getElementById('btn_ganjil').onclick = function () {
+    var input_num = document.getElementById('input_angka').value;
+
+    if (input_num === '') {
+        txt_hasil.innerHTML('Input tidak boleh kosong');
+    } else if (isNaN(input_num)) {
+        txt_hasil.innerHTML('Input harus berupa angka');
+    } else {
+        $.ajax({
+            url: base_url + '/api/ganjil',
+            type: 'GET',
+            dataType: 'json',
+            data: input_num,
+            success: (data) => txt_hasil.innerHTML(data)
+        })
+    }    
+}
+
+document.getElementById('btn_prima').onclick = function () {
+    var input_num = document.getElementById('input_angka').value;
+
+    if (input_num === '') {
+        txt_hasil.innerHTML('Input tidak boleh kosong');
+    } else if (isNaN(input_num)) {
+        txt_hasil.innerHTML('Input harus berupa angka');
+    } else {
+        $.ajax({
+            url: base_url + '/api/prima',
+            type: 'GET',
+            dataType: 'json',
+            data: input_num,
+            success: (data) => txt_hasil.innerHTML(data)
+        })
+    }
 }
